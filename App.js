bba0e04972391888b042877d2e571d1eb7951049
dylanalvarez1 <dylanalvarez1@ufl.edu>
2018-01-20 04:34:49 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
+
+
+export default class App extends React.Component {
+  
+myPress = () => {
+	
+};
+
+ randomHex = () => {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+render() {
+    return (
+      <View style={styles.container}>
+        <Text >Welcome to our hack</Text>
+	<Text> </Text>
+       
+
+
+<Button
+  onPress={this.myPress}
+  title="Find a Store"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#bafcd6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+});
