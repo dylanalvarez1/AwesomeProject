da59969344d689b445c91fa199be61e004ab31fe
Mateo Delgadillo <delgadillomateo@users.noreply.github.com>
2018-01-20 07:49:30 -0500

    Updated
    latest build

--- App.js
+++ App.js
@@ -1,6 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Screen, Spinner } from 'react-native';
 import { Button } from 'react-native';
+
+// import { Screen, Spinner, Examples } from '@shoutem/ui';
+// import { stringify as queryString } from 'query-string';
+
+// import RecommendationsMap from './RecommendationsMap';
+// import { OverlayTopics, BottomTopics } from './Topics';
+
+const API_DEBOUNCE_TIME = 2000;
+
+class locationService extends Component {
+  state = {
+    mapRegion: null,
+    gpsAccuracy: null,
+    recommendations: [],
+    lookingFor: null,
+    headerLocation: null,
+    last4sqCall: null
+  }
+  watchID = null
+
+  componentWillMount() {
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      let region = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: 0.00922*1.5,
+        longitudeDelta: 0.00421*1.5
+      }
+      this.onRegionChange(region, position.coords.accuracy);
+    });
+  }
+
+  componentWillMount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  onRegionChange(region, gpsAccuracy) {
+    this.setState({
+      mapRegion: region,
+      gpsAccuracy: gpsAccuracy || this.state.gpsAccuracy
+    });
+  }
+
+  render() {
+    const { mapRegion, lookingFor } = this.state;
+
+    if (mapRegion) {
+      return (
+        <Screen style = {styles.centered}>
+          <Text> {mapRegion.latitude}, {mapRegion.longitude}</Text>
+        </Screen>
+      );
+    }else{
+      return (
+        <Screen style={styles.centered}>
+          <Spinner />
+        </Screen>
+      );
+    }
+  }
+}
 
 class GeolocationExample extends Component {
   constructor(props) {
@@ -41,7 +102,8 @@ class GeolocationExample extends Component {
 export default class App extends React.Component {
 
 myPress = () => {
-  console.log('Button pressed')
+  console.log('Button was pressed')
+  console.log()
 };
 
  randomHex = () => {
